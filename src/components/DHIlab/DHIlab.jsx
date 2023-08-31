da9e03ed434beeff9ed994ef10d2156c2ca1e80e
bodiwael <bodiwael999@gmail.com>
2023-08-31 23:19:32 +0300

    first commit

---
+++ src/components/DHIlab/DHIlab.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./DHIlab.css";
+class DHIlabLogo extends Component {
+    render() {
+        return (<React.Fragment>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <circle cx="50" cy="50" r="50" fill="#0b5586" />
+          <path d="M0 50c0 27.614 22.386 50 50 50s50-22.386 50-50" fill="#0b5586" />
+        </svg>
+
+        {/* <div> */}
+        {/* </div> */}
+      </React.Fragment>);
+    }
+}
+export default DHIlabLogo;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import DHIlab from "./DHIlab/DHIlab";
+
+const Navbar = () => {
+  let isArrayFunction = (inputArray) => {
+    if (Array.isArray(inputArray) && inputArray.length !== 0) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+  return (
+    <div id="Navbar">
+      <div class="usa-overlay"></div>
+      <header class="usa-header usa-header--basic usa-header--megamenu padding-bottom-4 padding-top-6">
+        <div class="usa-nav-container">
+          <div class="usa-navbar">
+            <div class="usa-logo margin-0" id="basic-mega-logo">
+              <em class="usa-logo__text">
+                <div className="display-flex flex-row align-items-center">
+                  <DHIlab />
+                  <h3 className="display-inline-block padding-left-1">EMI Symptoms Checker</h3>
+                </div>
+              </em>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
