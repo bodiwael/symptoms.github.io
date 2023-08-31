5219534f937c9742995de7e6231af3f50dd1dcfa
bodiwael <bodiwael999@gmail.com>
2023-09-01 00:08:23 +0300

    Updates

---
+++ precache-manifest.242b82220d3dce29b5b0f89d5dc90fb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964d736222a65dec5a2b8c5c71c39d5c",
+    "url": "/symptoms.github.io/index.html"
+  },
+  {
+    "revision": "4802ae639fdfbfaf184c",
+    "url": "/symptoms.github.io/static/css/main.26c8b47a.chunk.css"
+  },
+  {
+    "revision": "6155eedf16369d755485",
+    "url": "/symptoms.github.io/static/js/2.263ac821.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/symptoms.github.io/static/js/2.263ac821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4802ae639fdfbfaf184c",
+    "url": "/symptoms.github.io/static/js/main.3f8aef18.chunk.js"
+  },
+  {
+    "revision": "0747c9be637fad7c8597",
+    "url": "/symptoms.github.io/static/js/runtime-main.c6685fab.js"
+  },
+  {
+    "revision": "1d25eaf26787a9d8a1ae23c5ddc82590",
+    "url": "/symptoms.github.io/static/media/logo.1d25eaf2.svg"
+  }
+]);
